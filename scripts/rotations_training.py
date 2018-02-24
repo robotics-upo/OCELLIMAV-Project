238e21de60756e8a2d5147ed476046949e3c9a60
Macarena M.F <36265705+macarena1807@users.noreply.github.com>
2018-02-24 23:17:15 +0100

    Update rotations_training.py

--- scripts/rotations_training.py
+++ scripts/rotations_training.py
@@ -7,9 +7,9 @@
 # This proyect was partially supported by MINECO (Spain) grant OCELLIMAV (TEC-61708-EXP). 
 # For more information about this work please check paper:
 #
-# "Bioinspired Vision-Only Attitude Rate Estimation Using Machine-Learning" by M.Mérida-Floriano, F.Caballero, 
-# D.García-Morales, L.Casares and L.Merino, submmited to the 2018 IEEE/RSJ International Conference on Intelligent 
-# Robots and Systems (IROS 2018)
+# "Bioinspired Direct Visual Estimation of Attitude Rates with Very Low Resolution Images using Deep Networks" by 
+# M.Mérida-Floriano, F.Caballero, D.García-Morales, L.Casares and L.Merino, submmited to the 2018 IEEE/RSJ International 
+# Conference on Intelligent Robots and Systems (IROS 2018)
 #
 # In this script we implement the first network described in the paper above. First, pure rotations datasets are 
 # loaded for training the network. Then. CNN's architecture is defined and trained.
